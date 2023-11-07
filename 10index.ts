927978d46a01eda1431a7149e0503574340d8eac
Umm E Rubab <142536921+UmmERubab27@users.noreply.github.com>
2023-11-08 00:51:55 +0500

    Add files via upload

---
+++ 10index.ts
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+
+const userInput =  await inquirer.prompt([
+    {
+        type: "number",
+        name: "minutes",
+        message: "Enter minutes in number"
+    },
+    {
+        type: "number",
+        name: "seconds",
+        message: "Enter seconds in number"
+    }
+]) 
+
+
+if (isNaN(userInput.minutes) || isNaN(userInput.seconds)){
+   console.log("please insert valid number")
+}
+
+
+async function Timer(minutes:number , seconds: number) {
+    console.log(`TIME INSERT = ${minutes} minutes and ${seconds} seconds`)
+
+    const totalSeconds = minutes * 60 + seconds;
+    let remainingSeconds = totalSeconds;
+
+        // create a setInterval function to start timer 
+    const timerInterval =  setInterval(() => {
+            const minutesRemaining = Math.floor(remainingSeconds / 60);
+            const secondsRemaining = remainingSeconds % 60;
+            console.log(`Time Remaining: ${minutesRemaining} minutes ${secondsRemaining} seconds`);
+            remainingSeconds--;
+
+            // create clearIntervel function to stop the setIntervel Functions
+            if (remainingSeconds < 0) {
+
+            clearInterval(timerInterval);
+            console.log("Timer's up!");
+            console.log(chalk.bgGreenBright`Developed by Rubab Nasir`);
+
+            }
+    }, 1000);
+}
+
+Timer(userInput.minutes, userInput.seconds)
+      
