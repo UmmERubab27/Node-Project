56a16196bd1e261610d38ab96d57898c12132154
Umm E Rubab <142536921+UmmERubab27@users.noreply.github.com>
2023-11-08 00:15:33 +0500

    Add files via upload

---
+++ cli.js
@@ -0,0 +1,86 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+let sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000);
+    });
+};
+async function welcome() {
+    let rainbowTitle = chalkAnimation.rainbow(`let start calculation       \n\n       Developed By Rubab Nasir`); //start
+    await sleep();
+    rainbowTitle.stop(); //stop after 2sec
+    console.log(`
+     _____________________
+    |  _________________  |
+    | | JO           0. | |
+    | |_________________| |
+    |  ___ ___ ___   ___  |
+    | | 7 | 8 | 9 | | + | |
+    | |___|___|___| |___| |
+    | | 4 | 5 | 6 | | - | |
+    | |___|___|___| |___| |
+    | | 1 | 2 | 3 | | x | |
+    | |___|___|___| |___| |
+    | | . | 0 | = | | / | |
+    | |___|___|___| |___| |
+    |_____________________|`);
+}
+;
+await welcome();
+async function askQuestion() {
+    const answers = await inquirer
+        .prompt([
+        /* pass your Question in here */
+        {
+            type: "input",
+            name: "ask",
+            message: "(User) What is your name? \n\n",
+        },
+        {
+            type: "number",
+            name: "number1",
+            message: "Enter your frist number? \n\n ",
+        },
+        {
+            type: "list",
+            name: "operator",
+            message: "Which operation you want to perform? \n\n ",
+            choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+        },
+        {
+            type: "number",
+            name: "number2",
+            message: "Enter your second number? \n\n",
+        },
+    ]);
+    if (answers.operator === "Addition") {
+        console.log(chalk.green(`${answers.number1} + ${answers.number2} = ${answers.number1 + answers.number2}`));
+    }
+    else if (answers.operator === "Subtraction") {
+        console.log(chalk.greenBright(`${answers.number1} - ${answers.number2} = ${answers.number1 - answers.number2}`));
+    }
+    else if (answers.operator === "Multiplication") {
+        console.log(chalk.green(`${answers.number1} * ${answers.number2} = ${answers.number1 * answers.number2}`));
+    }
+    else if (answers.operator === "Division") {
+        console.log(chalk.green(`${answers.number1} / ${answers.number2} = ${answers.number1 / answers.number2}`));
+    }
+    ;
+}
+;
+//askQuestion();
+async function startAgain() {
+    do {
+        await askQuestion();
+        var again = await inquirer
+            .prompt({
+            type: "input",
+            name: "restart",
+            message: " Do you want to continue? Press (Y/N)? ",
+        });
+    } while (again.restart == `y` || again.restart == `Y` || again.restart == `yes` || again.restart `YES`);
+}
+;
+startAgain();
---
+++ index.js
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+import chalkanimation from "chalk-animation";
+console.clear();
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000);
+    });
+};
+async function welcome() {
+    let rainbowTitle = chalkanimation.rainbow('=========>>>Welcome to RUBAB-BANK<<<=========');
+    await sleep();
+    rainbowTitle.stop();
+}
+await welcome();
+class Transaction {
+    AccNo;
+    AccHolderName;
+    Balance;
+    constructor(accno, name, bal) {
+        this.AccNo = accno;
+        this.AccHolderName = name;
+        this.Balance = bal;
+    }
+    BalanceCheck() {
+        console.log('--------------------------COUNTER BALANCE CHECK--------------------------');
+        if (this.Balance < 0) {
+            console.log('Insufficient Balance Please Deposit');
+        }
+        else {
+            console.log(chalk.bgRed.bold('Your Account Balance is: ' + this.Balance));
+        }
+    }
+    Deposit(amount) {
+        console.log('--------------------------COUNTER CASH DEPOSIT--------------------------');
+        this.Balance += amount;
+        console.log('Amount of ' + amount + " " + 'Deposit Successfully');
+        console.log(chalk.bgBlue.bold('Your Current Balance is: ' + this.Balance));
+    }
+    Withdraw(amount) {
+        console.log('--------------------------COUNTER CASH WITHDRAW--------------------------');
+        if (amount > this.Balance) {
+            console.log(chalk.bgRed.bold('Not Enough Balance'));
+        }
+        else {
+            this.Balance -= amount;
+            console.log('Amount of ' + amount + ' Withdrawn Successfully');
+            console.log(chalk.bgYellow.bold('Your Current Balance is: ' + this.Balance));
+        }
+    }
+}
+let user1 = new Transaction(786, "Vj Kazim", 0);
+user1.BalanceCheck();
+user1.Deposit(100000);
+user1.Withdraw(50000);
+user1.BalanceCheck();
